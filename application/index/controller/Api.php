d73171c9a6a60348656f67353aef3f759136aad6
lzb <514715401@qq.com>
2018-08-13 22:08:48 +0800

    qwe

--- application/index/controller/Api.php
+++ application/index/controller/Api.php
@@ -63,18 +63,27 @@ class Api extends Controller
      *
      * @param name: type:int require:1 default:1 other: desc:
      *
-     * @return id:标签ID
-     * @return name:标签名称
-     * @return tag_id:标签属性@
-     * @tag_id tag_id:标签ID id:属性ID  name:属性名称 
+     * @return id:房源ID
+     * @return title:房源名称
+     * @return cat_id:房源ID
+     * @return father:父级数据@
+     * @father id:父级ID  name:父级名称
+     * @return matching:配套数据@
+     * @matching id:配套ID  name:配套名称 
      */
-   //  public function Source(){
-   //      $List=db('source')->where('display',1)->order('listorder asc')->select();
-   //      foreach ($List as $key => $value) {
-   //          $tag_id=db('tag_attr')->where('tag_id','in',$value['id'])->select();
-   //          $List[$key]['tag_id']=$tag_id;    
-   //      }
-   //      $this->result($List,200,'调用成功!','JSON');
-   // }
+    public function Source(){
+        $source = db('source')->where('display','eq',0 and 'recommend',1)->select();
+            //遍历数据获取父级的数据
+            foreach ($source as $key => $value) {
+                $type = db('cate')->where('id','in',$value['cat_id'])->select();
+                $source[$key]['father'] = $type;
+            }
+            // 配套数据
+            foreach ($source as $k => $v) {
+                $dz = db('matching')->where('id','in',$v['matching'])->select();
+                $source[$k]['matching'] = $dz;
+            }
+        $this->result($source,200,'调用成功!','JSON');
+   }
    
 }
